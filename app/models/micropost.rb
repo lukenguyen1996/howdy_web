48da666d3a37d032f3dc8cd355b6223f6c62501e
LukeNguyen <minhduc.nguyen@centre.edu>
2017-07-27 15:45:56 -0500

    Deploy attempt-1

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,10 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> {order(created_at: :desc)}
-  mount_uploader :picture, PictureUploader
+  #mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
-  validate  :picture_size
+  #validate  :picture_size
 
 
   private
