63b40a74472fcef660918eb3f5e108137e044bb6
LukeNguyen <minhduc.nguyen@centre.edu>
2017-07-28 08:04:54 -0500

    change url for heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'howdy-web.herokuapp.com'
+  host = 'enigmatic-forest-66276.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
